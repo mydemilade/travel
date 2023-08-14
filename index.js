3ef91f315ddfbe42d23c39773830628977933cdf
mydemilade <ayunoluwademilade@gmail.com>
2023-08-14 14:39:39 +0100

    hi

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
 let searchBtn = document.querySelector('#search-btn');
 let searchBar = document.querySelector('.search-bar-container');
 let formBtn = document.querySelector('#login-btn')
@@ -5,6 +6,7 @@ let loginForm = document.querySelector('.login-form-container');
 let formClose = document.querySelector('#form-close')
 let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar')
+let videoBtn = document.querySelectorAll('.vid-btn');
 
 
 window.onscroll =() =>{
@@ -28,3 +30,16 @@ formBtn.addEventListener('click', () =>{
 formClose.addEventListener('click', () =>{
     loginForm.classList.remove('active')
 });
+
+videoBtn.forEach(btn =>{
+    btn.addEventListener('click', () =>{
+        document.querySelector('.controls.active')
+        .classList.remove('active');
+        btn.classList.add('active');
+        let src = btn.getAttribute('data-src');
+        document.querySelector('#video-slider').src =src;
+        
+    })
+    console.log(videoBtn)
+})
+})
