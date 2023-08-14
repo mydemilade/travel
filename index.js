c2e4234e8384759b4618d040e47d63f62f26f3c4
mydemilade <ayunoluwademilade@gmail.com>
2023-08-14 23:22:55 +0100

    more styling

--- index.js
+++ index.js
@@ -43,3 +43,22 @@ videoBtn.forEach(btn =>{
     console.log(videoBtn)
 })
 })
+// var swiper = new Swiper('.review-slider', {
+//     spaceBetween:20,
+//     loop:true,
+//     auto: {
+//         delay: 2500,
+//         disableOnInteraction: false,
+//     },
+//     breakpoints:{
+//         640:{
+//             slidesPerView: 1,
+//         },
+//         768 :{
+//             slidesPerView: 2,
+//         },
+//         1024: {
+//             slidesPerView:3,
+//         },
+//     }
+// })
